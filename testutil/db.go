2a5e818b15f37c5835c445084f48d658fad2eb32
pdj107 <cxz541@gmail.com>
2024-03-27 20:28:04 +0900

    Fix: 테스트 DB IP 변경

--- testutil/db.go
+++ testutil/db.go
@@ -17,8 +17,7 @@ func OpenDBForTest(t *testing.T) *sqlx.DB {
 	}
 	db, err := sql.Open(
 		"mysql",
-		// fmt.Sprintf("todo:todo@tcp(127.0.0.1:%d)/todo?parseTime=true", port),
-		fmt.Sprintf("todo:todo@tcp(192.168.10.51:%d)/todo?parseTime=true", port),
+		fmt.Sprintf("todo:todo@tcp(127.0.0.1:%d)/todo?parseTime=true", port),
 	)
 	if err != nil {
 		t.Fatal(err)
